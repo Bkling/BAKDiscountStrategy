62c0c3681cb73d81be2420d005bfce1e98d51fd7
bkling <bkling@L11715038604.wctc.msft>
2013-02-28 17:51:09 -0600

    Finished adding all JAVA DOC comments to methods, also finished putting a date stamp on the receipt when the program is run.

--- src/bakdiscountstrategy/Receipt.java
+++ src/bakdiscountstrategy/Receipt.java
@@ -2,10 +2,11 @@ package bakdiscountstrategy;
 
 import java.text.SimpleDateFormat;
 import java.util.*;
+
 /**
  * This class represents a receipt in a business.
- * @author Benjamin Kling, bkling@my.wctc.edu
- * version 1.00
+ *
+ * @author Benjamin Kling, bkling@my.wctc.edu, version 1.00
  */
 public class Receipt {
 
@@ -32,6 +33,12 @@ public class Receipt {
     };
 
     //Sort through the Customer Array
+    /**
+     * This method gets the customer id
+     *
+     * @param custID - uses the customer id
+     * @return - a valid customer id
+     */
     public final Customer getCustomer(String custID) {
         Customer customer = null;
         for (Customer c : customers) {
@@ -43,25 +50,51 @@ public class Receipt {
         return customer;
     }
 
+    /**
+     * This method sets a customer using an argument
+     *
+     * @param customer - a valid customer
+     */
     public final void setCustomer(Customer customer) {
         //Needs Validation
         this.customer = customer;
     }
 
+    /**
+     * This method gets the line items
+     *
+     * @return - multiple line items
+     */
     public final LineItem[] getLineItems() {
         return lineItems;
     }
 
+    /**
+     * This method sets the line items using an argument
+     *
+     * @param lineItems - uses valid line items
+     */
     public final void setLineItems(LineItem[] lineItems) {
         //Needs Validation
         this.lineItems = lineItems;
     }
 
+    /**
+     * This method adds line items to an array
+     *
+     * @param prodID - uses the product id to add
+     * @param qty - uses the quantity to add
+     */
     public final void addLineItem(String prodID, int qty) {
         LineItem item = new LineItem(prodID, qty);
         addItemToArray(item);
     }
 
+    /**
+     * This method adds items to an array
+     *
+     * @param item - adds valid line items
+     */
     private final void addItemToArray(LineItem item) {
         LineItem[] tempItems = new LineItem[lineItems.length + 1];
         System.arraycopy(lineItems, 0, tempItems, 0, lineItems.length);
@@ -69,6 +102,11 @@ public class Receipt {
         lineItems = tempItems;
     }
 
+    /**
+     * This method gets the total before discount
+     *
+     * @return - a valid total before
+     */
     public final double getTotalBeforeDiscount() {
         double total = 0.0;
         for (LineItem item : lineItems) {
@@ -77,6 +115,11 @@ public class Receipt {
         return total;
     }
 
+    /**
+     * This method gets the total after discount
+     *
+     * @return - a valid total after
+     */
     public final double getTotalAfterDiscount() {
         double total = 0.0;
         for (LineItem item : lineItems) {
@@ -85,6 +128,11 @@ public class Receipt {
         return total;
     }
 
+    /**
+     * This method gets the display
+     *
+     * @return - a valid string of data
+     */
     public final String getDisplay() {
         String data;
         data = "Welcome to Kohl's. Expect Great Things \n";
