137c850d4bcfd83ec8c12e4de370785855d82395
bkling <bkling@L11715038604.wctc.msft>
2013-02-28 17:40:14 -0600

    Finished the Qty Discount class java doc comments.

--- src/bakdiscountstrategy/Product.java
+++ src/bakdiscountstrategy/Product.java
@@ -1,8 +1,9 @@
 package bakdiscountstrategy;
+
 /**
  * This class represents a product that a business would sell.
- * @author Benjamin Kling, bkling@my.wctc.edu
- * version 1.00
+ *
+ * @author Benjamin Kling, bkling@my.wctc.edu, version 1.00
  */
 public class Product {
 
@@ -21,42 +22,88 @@ public class Product {
     }
 
     //Getters and Setters
+    /**
+     * This method gets a product id
+     *
+     * @return - a product id
+     */
     public final String getProductID() {
         return productID;
     }
 
+    /**
+     * This method sets a product id using an argument
+     *
+     * @param productID - a valid product id
+     */
     public final void setProductID(String productID) {
         //Needs Validation
         this.productID = productID;
     }
 
+    /**
+     * This method gets the product price
+     *
+     * @return - a product price
+     */
     public final double getProductPrice() {
         return productPrice;
     }
 
+    /**
+     * This method sets a product price using an argument
+     *
+     * @param productPrice - a valid product price
+     */
     public final void setProductPrice(double productPrice) {
         //Needs Validation
         this.productPrice = productPrice;
     }
 
+    /**
+     * This method gets a description of a product
+     *
+     * @return - a product description
+     */
     public final String getDescription() {
         return description;
     }
 
+    /**
+     * This method sets a product description using an argument
+     *
+     * @param description - a valid product description
+     */
     public final void setDescription(String description) {
         //Needs Validation
         this.description = description;
     }
 
+    /**
+     * This method gets a discount strategy
+     *
+     * @return - a discount strategy
+     */
     public final DiscountStrategy getDiscountStrategy() {
         return discountStrategy;
     }
 
+    /**
+     * This method sets a discount strategy using an argument
+     *
+     * @param DiscountStrategy - a valid discount strategy
+     */
     public final void setDiscountStrategy(DiscountStrategy DiscountStrategy) {
         //Needs Validation
         this.discountStrategy = DiscountStrategy;
     }
 
+    /**
+     * This method gets the discount of a product
+     *
+     * @param qty - uses a quantity amount to determine product price
+     * @return - a discount amount
+     */
     public final double getDiscount(int qty) {
         return (discountStrategy.getDiscountAmt(productPrice, qty));
     }
--- src/bakdiscountstrategy/QtyDiscountStrategy.java
+++ src/bakdiscountstrategy/QtyDiscountStrategy.java
@@ -1,8 +1,9 @@
 package bakdiscountstrategy;
+
 /**
  * This class implements the discount strategy.
- * @author Benjamin Kling, bkling@my.wctc.edu
- * version 1.00
+ *
+ * @author Benjamin Kling, bkling@my.wctc.edu, version 1.00
  */
 public class QtyDiscountStrategy implements DiscountStrategy {
 
@@ -11,6 +12,13 @@ public class QtyDiscountStrategy implements DiscountStrategy {
     private int minQty = 3;
     private double noDiscount = 0.0;
 
+    /**
+     * This method overrides the discount strategy interface
+     *
+     * @param productPrice - uses a product price to determine discount
+     * @param qty - uses a quantity to determine discount
+     * @return - a discount amount
+     */
     @Override
     public double getDiscountAmt(double productPrice, int qty) {
         if (qty >= minQty) {
@@ -21,19 +29,41 @@ public class QtyDiscountStrategy implements DiscountStrategy {
     }
 
     //Getters and Setters
+    /**
+     * This method gets a percent
+     *
+     * @return - a percentage
+     */
     public double getPer() {
         return per;
     }
 
+    /**
+     * This method sets a percent using an argument
+     *
+     * @param per - a valid percentage
+     */
     public void setPer(double per) {
+        //Needs Validation
         this.per = per;
     }
 
+    /**
+     * This method gets a minimum quantity
+     *
+     * @return - a minimum quantity
+     */
     public int getMinQty() {
         return minQty;
     }
 
+    /**
+     * This method sets a minimum quantity using an argument
+     *
+     * @param minQty - a valid minimum quantity
+     */
     public void setMinQty(int minQty) {
+        //Needs Validation
         this.minQty = minQty;
     }
 }
